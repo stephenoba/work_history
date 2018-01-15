c508dee89200f9456e825ac38fefe75a50853e23
Way2nnadi <ucheunnadi@gmail.com>
2018-01-14 17:35:51 -0800

    [add] week3 assessment

---
+++ Assessments/Week3/function.js
@@ -0,0 +1,36 @@
+/**
+  In this assessment, you will be creating new Javascript functions.
+  Make sure to read the directions to build the functions carefully.
+  You should use the below pattern to below to write your functions:
+  ```
+  function {{name_of_function}} {
+    // Write your code here
+  }
+
+  ```
+*/
+
+// 1. write a function that takes a number as a parameter and returns it as a
+// string
+
+// 2. write a function that takes a number as a parameter and returns that
+// number subtracted by 1
+
+// 3. write a function that takes a number as a parameter and returns that
+// number added by 1
+
+// 4. write a function that takes two number as its parameters and returns then
+// addition of those two numbers
+
+// 5. write a function that takes two number as its parameters and returns then
+// squared of those two numbers
+
+// 6. write a function that takes a number and returns true if the number is odd
+// and false if it is not
+
+// 7. write a function that takes two strings as its parameters and returns the
+// combination of those two strings
+
+// 8. write a function that takes the radius of a circle a parameter and returns
+// the Area of the circle.
+// For this exercise π (pie) can equal `3.14`
