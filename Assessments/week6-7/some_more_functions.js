ca918b9718b95976055d318cd21e747415d2f0a7
Way2nnadi <ucheunnadi@gmail.com>
2018-02-13 08:17:07 -0800

    [add] week 6-7 assessment

---
+++ Assessments/week6-7/some_more_functions.js
@@ -0,0 +1,28 @@
+/**
+  Its important that you keep practicing creating functions. Some here are more
+  that you will need to implement
+
+  ```
+  function {{name_of_function}} {
+    // Write your code here
+  }
+
+  OR
+
+  const {{name_of_function}} = () => {
+    // Write your code here
+  }
+
+*/
+
+// 1. write a function that takes a string as its first parameter. This function
+// counts the number of vowels in the string and returns that number.
+
+// 2. write a function that takes a string as it first parameter. This function
+// return a new string with the first 3 letters lower-cased. If the string is less
+// than 3 letters long, this function will return the new string with all letters
+// upper-cased.
+
+// 3. write a function that stakes a string as it first parameter. This function
+// returns the first half of the string if the string is even length. Otherwise,
+// it will return null;
