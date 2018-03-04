c9621d573bcf8ab602b5be7f25eef36cc5a7b10e
stephenoba <obastepheno@gmail.com>
2018-03-04 04:11:37 +0100

    assessment

--- Assessments/week6-7/some_more_functions.js
+++ Assessments/week6-7/some_more_functions.js
@@ -36,13 +36,22 @@ getVowels('Hello World')
 // 2. write a function that takes a string as it first parameter. This function
 // return a new string with the first 3 letters lower-cased. If the string is less
 // than 3 letters long, this function will return the new string with all letters
-// upper-cased.
+b// upper-cased.
 // EX1:
   // sample input: "HELLO"
   // sample output: "helLO"
 // EX2:
   // sample input: "he"
   // sample output: "HE"
+
+  function changeCase(string) {
+    if (string.length >= 3) {
+  return string.slice(0, 3).toLowerCase() + string.slice(3).toUpperCase();
+     } else if (string.length < 3) {
+   return  string.toUpperCase();
+  }
+}
+console.log(changeCase('Hello'));
 
 // 3. write a function that takes a string as it first parameter. This function
 // returns the first half of the string if the string is even length. Otherwise,
@@ -54,11 +63,35 @@ getVowels('Hello World')
   // sample input: "Hello World"
   // sample output: null
 
+  function printIfEven(string) {
+    var n = string.length / 2
+    if (string.length % 2 === 0) {
+      return string.substr(0, n)
+    } else if (string.length % 2 === 1) {
+      return null
+    }
+  }
+  console.log(printIfEven("HelloWorld"))
+
 // 4. write a function that takes an array of numbers as its first parameter.
 // This function returns the sum of the numbers in the array
 // EX:
   // sample input: [1, 7, 1, 4, 1, 6, 4, 5]
   // sample output: 29
+
+  function sumArray(array) {
+  for (
+    var
+      index = 0,
+      length = array.length,
+      sum = 0;
+      index < length;
+      sum += array[index++]
+  );
+  return sum;
+}
+
+console.log(sumArray([1, 2, 3, 6, 9]))
 
 // 5. write a function that takes an array of numbers as its first parameter.
 // This function returns an the number that is the most frequent in the array
